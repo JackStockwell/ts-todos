f799c88627ef0f10dcde0d06822e81839dffda6f
Jack Stockwell <95311730+JackStockwell@users.noreply.github.com>
2023-09-28 23:25:21 +0100

    able to fetch data from todos and map them

--- client/src/components/TodoList.tsx
+++ client/src/components/TodoList.tsx
@@ -1,24 +1,38 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { idbPromise } from '../utils/helpers';
 
 export type todo = {
-    id: number;
     title: string,
     text: string, 
     completed: boolean;
+    id?: number;
 }
 
 
 const TodoList = () => {
 
     const todoState: todo[] = [
-        {id: 1, title: "Test", text: "Test Todo", completed: false},
-        {id: 2, title: "TWO", text: "Test TWO-DO", completed: true}
+        {title: "Test", text: "Test Todo", completed: false},
     ]
 
     const [todos, setTodos] = useState<todo[]>(todoState)
 
-    const handleToggle = (id: number) => {
+    const fetchData = async (): Promise<Array<todo> | string | undefined> => {
+
+        try {
+            const response: any = await idbPromise('todos', 'get')
+            setTodos(response)
+            return;
+
+        } catch (err) {
+            if (err) {
+                return err.message
+            }
+        }
+        
+    }
+
+    const handleToggle = (id: number | undefined) => {
         todos.map((todo) => {
             if (todo.id === id) {
                 return { ...todo, completed: !todo.completed }
@@ -27,21 +41,36 @@ const TodoList = () => {
         })
     }
 
+    // USE EFFECT, to retrieve data from indexDB, to be trigger when
+    // Todo is created,
+    // Upon page load.
+
     const handleOnSubmit = (e: any) => {
         e.preventDefault();
         const form = e.target;
         const formData = new FormData(form)
+        const title = formData.get('title')?.toString()
+        const text = formData.get('text')?.toString()
+
+        if (!title || !text) {
+            alert('Title or Text is not a valid data type!')
+            return;
+        }
 
         const newTodo: todo = {
-            id: 3,
-            title: formData.get('title') as string,
-            text: formData.get('text') as string,
+            title: title,
+            text: text,
             completed: false
         }
 
-        idbPromise('todos', 'put', newTodo);
-
+        idbPromise('todos', 'add', newTodo);
     }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    console.log(todos)
 
     return (
         <div>
